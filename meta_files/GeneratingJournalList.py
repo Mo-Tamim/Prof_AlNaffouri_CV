6873c1f20c00b5af57d295f324f409dba54c2443
Tamim <mohamd2020@hotmail.com>
2018-03-21 13:34:34 +0300

    Version 2
    * Publication layout were modified.
    * Patents were updated from KFUPM and KAUST.

--- meta_files/GeneratingJournalList.py
+++ meta_files/GeneratingJournalList.py
@@ -34,7 +34,8 @@ class PublicationList:
             bib_data = {}
             for line in bib_file:
                 line = line.strip()
-                if '@' not in line and '=' not in line and '}' not in line:
+                # if '@' not in line and '=' not in line and '}' not in line:
+                if '@' not in line and '}' not in line:
                     line1 += line
                     long_line = True
                     continue
@@ -132,14 +133,32 @@ class PublicationList:
             author_names = self.seperating_author_names(publication_data[item_key]['author'])
 
             if publication_data[item_key]['status'].lower() == 'published':
-                full_entry = '\item ' + author_names + \
-                             ' ``' + publication_data[item_key]['title'] + \
-                             '", in {\\em ' + journal_name + '}' +\
-                             ', ' + publication_date_formated + \
-                             ', Vol.' + publication_data[item_key].get('volume', '') + \
-                             ', pp.' + publication_data[item_key]['pages'] + \
-                             ', ' + publication_data[item_key]['month'] + \
-                             '. ' + publication_data[item_key]['year'] + '.'
+
+                if publication_data[item_key].get('volume', '') != '':
+                    publication_volum = ', vol.' + publication_data[item_key].get('volume')
+                else:
+                    publication_volum = ''
+                if publication_data[item_key].get('pages', '') != '':
+                    publication_page =  ', pp.' + publication_data[item_key]['pages']
+                else:
+                    publication_page = ''
+
+                if "Conference" in self.bib_name:
+                    full_entry = '\item ' + author_names + \
+                                 ' ``' + publication_data[item_key]['title'] + \
+                                 '", {\\em ' + journal_name + '}' +\
+                                 publication_volum +\
+                                 publication_page + \
+                                 '. ' + publication_data[item_key]['year'] + '.'
+                else:
+                    full_entry = '\item ' + author_names + \
+                                 ' ``' + publication_data[item_key]['title'] + \
+                                 '", in {\\em ' + journal_name + '}' +\
+                                 publication_volum +\
+                                 publication_page + \
+                                 ', ' + publication_data[item_key]['month'] + \
+                                 '. ' + publication_data[item_key]['year'] + '.'
+
             else:
                 full_entry = '\item ' + author_names + \
                              ' ``' + publication_data[item_key]['title'] + \
@@ -156,25 +175,70 @@ class PublicationList:
         txt_file.close()
 
 
+    def CreateCSV(self):
+        # pdb.set_trace()
+
+        BIBKeys = [Key for Key in self.bib_data]
+        CSVHeader = ["ItemKey"]
+        for HeaderItem in self.bib_data[BIBKeys[1]]:
+            CSVHeader.append(HeaderItem)
+
+
+        with open('CSVTemp.csv', 'w', encoding='utf-8', newline='') as CSVFilePointer:
+            CSVWriterPointer = csv.writer(CSVFilePointer)
+            for BibKey in self.bib_data:
+                CSVLineContent = []
+                # This loop take an item from the CSV and put its value in the CSV line
+                for HeaderItem in CSVHeader:
+                    if HeaderItem == "ItemKey":
+                        CSVLineContent.append(BibKey)
+                        continue
+                    if HeaderItem in self.bib_data[BibKey]:
+                        CSVLineContent.append(self.bib_data[BibKey][HeaderItem])
+                    else:
+                        CSVLineContent.append('')
+
+                # This loop search for a new head item an add it to the CSV head
+                for HeaderItem in self.bib_data[BibKey]:
+                    if HeaderItem not in CSVHeader:
+                        CSVHeader.append(HeaderItem)
+                        CSVLineContent.append(self.bib_data[BibKey][HeaderItem])
+
+                CSVWriterPointer.writerow(CSVLineContent)
+
+        # print(CSVFilePointer.closed)
+
+        with open('CSVTemp.csv') as CSVFilePointer:
+            CSVReaderPointer = csv.reader(CSVFilePointer)
+            with open(self.csv_name, 'w+', newline='') as CSVFilePointer2:
+                CSVWriterPointer = csv.writer(CSVFilePointer2)
+                CSVWriterPointer.writerow(CSVHeader)
+                for Row in CSVReaderPointer:
+                    CSVWriterPointer.writerow(Row)
+        os.remove('CSVTemp.csv')
+
+
 
 
 def main():
-    journal_bib_name = "JournalPublications V5.bib"
+    journal_bib_name = "JournalPublications V6.bib"
     journal_json_name = 'JournalPublications V3.json'
     journal_csv_name = 'JournalPublications V3.csv'
     journal_publication_obj = PublicationList(journal_json_name, journal_csv_name, journal_bib_name)
     journal_publication_data = journal_publication_obj.read_bib_file()
     all_sorted_pub = journal_publication_obj.sort_by_date
     journal_publication_obj.writing_to_tex('AllJournalPublications.txt')
+    journal_publication_obj.CreateCSV()
     print('\n \n The total journal publications found is   ====> ' + str(len(journal_publication_data.keys())))
 
-    conference_bib_name = "ConferencePublications V4.bib"
+    conference_bib_name = "ConferencePublications V6.bib"
     conference_json_name = 'ConferencePublications V3.json'
     conference_csv_name = 'ConferencePublications V3.csv'
     conference_publication_obj = PublicationList(conference_json_name, conference_csv_name, conference_bib_name)
     conference_publication_data = conference_publication_obj.read_bib_file()
     all_sorted_pub = conference_publication_obj.sort_by_date
     conference_publication_obj.writing_to_tex('AllConferencePublications.txt')
+    conference_publication_obj.CreateCSV()
     print('\n \n The total conference publications found is   ====> ' + str(len(conference_publication_data.keys())))
 
 
